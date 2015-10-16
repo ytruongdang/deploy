6df8105e70e6a7799dbd214b3cf0fc43241fe918
ytruongdang <ytruongdang@gmail.com>
2015-10-16 16:50:32 +0700

    capitrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,7 @@
 lock '3.4.0'
 
 set :application, 'deploy'
-set :repo_url, 'git@example.com:ytruongdang/deploy.git'
-
+set :repo_url, 'git@github.com:ytruongdang/deploy.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
